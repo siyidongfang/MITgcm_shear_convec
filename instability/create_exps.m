6eba265839d1290a099037252b796c846e2f1f25
siyidongfang <y_si@mit.edu>
2023-12-10 09:44:31 -0500

    update

--- instability/create_exps.m
+++ instability/create_exps.m
@@ -10,7 +10,7 @@ N = 1e-3;
 topo = 4;
 Ptide = 43200;
 dz = 0.006;             % dimensionless vertical grid spacing
-dt = 0.0002;
+dt = 0.001;
 m1km = 1000;
 
 Hdepth = 1500;
@@ -22,7 +22,7 @@ Shear_parm = [1e-20 0.05 0.2 0.4 0.6 0.8 1 1.2 1.4 1.6 1.8 2 2.2 2.4]*1e-3;
 lambda_parm = [0.001 0.0025 0.005 0.0075 0.01 0.025 0.05 0.075 0.1 0.25 0.5 0.75 1 2.5 5 7.5 10 25 50 75 100 250 500 1000 1e7 1e47 1e97]*m1km;
 
 % for Nexp =22:length(lambda_parm)
-for Nexp = 1:3
+for Nexp = 4:6
     % lambda = 750;
     lambda = lambda_parm(Nexp)
     kx = 2*pi/(lambda/delta);
