b5775471fd415afee8e562d7514a6a040c68298e
siyidongfang <y_si@mit.edu>
2024-03-12 15:41:22 -0400

    update

--- instability/download_engaging.sh
+++ instability/download_engaging.sh
@@ -1,1 +1,1 @@
-rsync -av --exclude "*.mat" --update y_si@eofe7.mit.edu:/nobackup1c/users/y_si/MITgcm_shear_convec/instability/* . 
+rsync -av --update y_si@eofe7.mit.edu:/nobackup1c/users/y_si/MITgcm_shear_convec/instability/* . 
