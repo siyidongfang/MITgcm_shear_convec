fe5586dcc2bb78e98888f109e3bb035c16c0327c
siyidongfang <y_si@mit.edu>
2024-06-25 13:33:56 -0400

    update

--- figures/Fig1.m
+++ figures/Fig1.m
@@ -1,24 +1,17 @@
 
-% clear;close all;
+clear;close all;
 addpath ../analysis/colormaps/
 addpath freezeColors/
 fontsize = 16;
 load_colors;
 
+calc_Fig1;
 
 figure(1)
 clf;   
 set(gcf,'Color','w');
 scrsz = get(0,'ScreenSize');
 set(gcf,'Position',[0.03*scrsz(3) 0.3*scrsz(4) 1400 700]);
-
-%%%------ Data for plotting bathymetry
-addpath ../observations/topography/
-ncfname = 'blt_canyon_mb_qc.nc';
-lat = ncread(ncfname,'lat')';
-lon = ncread(ncfname,'lon')';
-z = ncread(ncfname,'z');
-load Rockall_gebco.mat
 
 %%% Canyon bathymetry
 ax2 = subplot('position',[0.04 0.53 0.27 0.25]);
@@ -82,63 +75,16 @@ axis tight;box off;
 set(gca,'FontSize',fontsize,'TickDir', 'in','TickLength',[0.1, 0.014]);
 title('Rockall Trough','FontSize',fontsize+3,'Position',[-5.5 57.5 -0.4]);
 xticks([-19:3:-6]);
-annotation('line','LineStyle','--','Color',black,'LineWidth',0.75,'Position', [0.04 0.54 0.18 0.34]);% %%% Observed velocity
-annotation('line','LineStyle','--','Color',black,'LineWidth',0.75,'Position', [0.31 0.765 -0.085 0.153]);% %%% Observed velocity
+annotation('line','LineStyle','--','Color',black,'LineWidth',0.75,'Position', [0.04 0.543 0.178 0.337]);
+annotation('line','LineStyle','--','Color',black,'LineWidth',0.75,'Position', [0.31 0.765 -0.085 0.153]);
 annotation('ellipse',[0.22 0.875 0.021 0.01],'Color',orange,'LineWidth',2,'rotation',70);
 % ylim([min(ele_lat) 59.2])
 freezeColors;
 
-%%
-
-
-%%% Observed velocity
-ax3 = subplot('position',[0.38 0.58 0.25 0.38]);
-annotation('textbox',[0.375 0.99 0.15 0.01],'String','C','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
-pcolor(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)');shading flat;
-xlabel('Time (hours)')
-ylabel('Depth (km)')
-set(gca,'Fontsize',fontsize);
-axis ij;
-clim([meanT-1 meanT+2])
-colormap(cmocean('balance'))
-title('Conservative temperature','Fontsize',fontsize+3);
-xticks([0:6:48])
-h3=colorbar(ax3);
-set(h3,'Position',[0.635  0.63 0.007 0.3]);
-set(get(h3,'Title'),'String','   (m/s)');
-
-
-%%% Linear-fit velocity
-ax4 = subplot('position',[0.715 0.58 0.25 0.38]);
-annotation('textbox',[0.71 0.99 0.15 0.01],'String','D','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
-pcolor(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)');shading flat;
-xlabel('Time (hours)')
-ylabel('Depth (km)')
-set(gca,'Fontsize',fontsize);
-axis ij;
-clim([meanT-1 meanT+2])
-colormap(cmocean('balance'))
-title('Conservative temperature','Fontsize',fontsize+3);
-xticks([0:6:48])
-h4=colorbar(ax4);
-set(h4,'Position',[0.97  0.63 0.007 0.3]);
-set(get(h4,'Title'),'String','   (m/s)');
-
-%------ Data for plotting temperature
-addpath ../observations/
-temp = ncread('mavs2_20210718_level1.nc','__xarray_dataarray_variable__');
-depth_temp = ncread('mavs2_20210718_level1.nc','depth')/1000;
-time_temp = double(ncread('mavs2_20210718_level1.nc','time'));
-time_temp = time_temp'/3600;%%% in hours
-time_temp(end)=48;
-% time_temp = time_temp/24;%%% in days
-plot_tidx = 1:20:length(time_temp);
-meanT = mean(temp,'all','omitnan');
-
 
 %%% Temperature
-ax5 = subplot('position',[0.045 0.07 0.25 0.38]);
-annotation('textbox',[0.025 0.475 0.15 0.01],'String','E','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
+ax3 = subplot('position',[0.045 0.07 0.25 0.38]);
+annotation('textbox',[0.025 0.482 0.15 0.01],'String','C','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
 pcolor(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)');shading flat;
 hold on;
 contour(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)',meanT-2:0.5:meanT+2,'Color',black);
@@ -151,14 +97,16 @@ clim([meanT-1 meanT+2])
 colormap(cmocean('balance'))
 title('Conservative temperature','Fontsize',fontsize+3);
 xticks([0:6:48])
-h5=colorbar(ax5);
-set(h5,'Position',[0.3 0.12 0.007 0.3]);
-set(get(h5,'Title'),'String','   (^oC)');
+h3=colorbar(ax3);
+set(h3,'Position',[0.3 0.12 0.007 0.3]);
+set(get(h3,'Title'),'String','   (^oC)');
+
+%%
 
 
-%%% Reconstructed dbdz using the observed velocity
-ax6 = subplot('position',[0.38 0.07 0.25 0.38]);
-annotation('textbox',[0.375 0.49 0.15 0.01],'String','F','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
+%%% Observed velocity
+ax4 = subplot('position',[0.38 0.58 0.25 0.38]);
+annotation('textbox',[0.375 0.99 0.15 0.01],'String','D','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
 pcolor(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)');shading flat;
 xlabel('Time (hours)')
 ylabel('Depth (km)')
@@ -166,7 +114,42 @@ set(gca,'Fontsize',fontsize);
 axis ij;
 clim([meanT-1 meanT+2])
 colormap(cmocean('balance'))
-title('Conservative temperature','Fontsize',fontsize+3);
+title('Observed velocity','Fontsize',fontsize+3);
+xticks([0:6:48])
+h4=colorbar(ax4);
+set(h4,'Position',[0.635  0.63 0.007 0.3]);
+set(get(h4,'Title'),'String','   (m/s)');
+
+
+%%% Linear-fit velocity
+ax5 = subplot('position',[0.715 0.58 0.25 0.38]);
+annotation('textbox',[0.71 0.99 0.15 0.01],'String','E','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
+pcolor(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)');shading flat;
+xlabel('Time (hours)')
+ylabel('Depth (km)')
+set(gca,'Fontsize',fontsize);
+axis ij;
+clim([meanT-1 meanT+2])
+colormap(cmocean('balance'))
+title('Linear-fit velocity','Fontsize',fontsize+3);
+xticks([0:6:48])
+h5=colorbar(ax5);
+set(h5,'Position',[0.97  0.63 0.007 0.3]);
+set(get(h5,'Title'),'String','   (m/s)');
+
+
+
+%%% Reconstructed dbdz using the observed velocity
+ax6 = subplot('position',[0.38 0.07 0.25 0.38]);
+annotation('textbox',[0.375 0.482 0.15 0.01],'String','F','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
+pcolor(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)');shading flat;
+xlabel('Time (hours)')
+ylabel('Depth (km)')
+set(gca,'Fontsize',fontsize);
+axis ij;
+clim([meanT-1 meanT+2])
+colormap(cmocean('balance'))
+title('Reconstructed \partial b/\partial z (observed)','Fontsize',fontsize+3);
 xticks([0:6:48])
 h6=colorbar(ax6);
 set(h6,'Position',[0.635 0.12 0.007 0.3]);
@@ -175,7 +158,7 @@ set(get(h6,'Title'),'String','   (^oC)');
 
 %%% Reconstructed dbdz using the linear-fit velocity
 ax7 = subplot('position',[0.715 0.07 0.25 0.38]);
-annotation('textbox',[0.71 0.49 0.15 0.01],'String','G','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
+annotation('textbox',[0.71 0.482 0.15 0.01],'String','G','FontSize',fontsize+3,'fontweight','normal','LineStyle','None');
 pcolor(time_temp(plot_tidx),depth_temp,temp(plot_tidx,:)');shading flat;
 xlabel('Time (hours)')
 ylabel('Depth (km)')
@@ -183,7 +166,7 @@ set(gca,'Fontsize',fontsize);
 axis ij;
 clim([meanT-1 meanT+2])
 colormap(cmocean('balance'))
-title('Conservative temperature','Fontsize',fontsize+3);
+title('Reconstructed \partial b/\partial z (linear-fit)','Fontsize',fontsize+3);
 xticks([0:6:48])
 h7=colorbar(ax7);
 set(h7,'Position',[0.97 0.12 0.007 0.3]);
