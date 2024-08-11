3f9805a6ed8b0e6ce3f403499f4b460defea820d
siyidongfang <y_si@mit.edu>
2024-08-11 16:05:41 -0400

    udpate

--- figures/Fig5_new.m
+++ figures/Fig5_new.m
@@ -12,32 +12,41 @@ set(gcf,'Position',[0.03*scrsz(3) 0.3*scrsz(4) 1000 350]);
 load('fig5/fig5_topo0_Ri.mat')
 ax1 = axes('position',[0.075 0.15+0.002 0.39 0.77]);
 annotation('textbox',[0.015 0.99+0.002 0.15 0.01],'String','a','FontSize',fontsize+3,'fontweight','bold','LineStyle','None');
-scatter(omega0_sort/omega,sqrt(epsilon_sort)/omega,20,grow_sort,'o', 'filled');
-clim([0 0.024])
+% scatter(omega0_sort/omega,sqrt(epsilon_sort)/omega,20,grow_sort,'o', 'filled');
+scatter(omega0_sort/omega,Ri_sort,20,grow_sort,'o', 'filled');
+clim([0 0.04])
 % colormap([[1 1 1];cmocean('phase')])
 colormap([[1 1 1]*0.97;jet])
 set(gca,'fontsize',fontsize)
-ylabel('$\sqrt\epsilon_0/\omega = \sqrt {\frac{2\omega_0^3}{\omega}\frac{\Lambda}{\tilde N}}\Big/\omega$','Interpreter','latex','FontSize',fontsize+4)
+set(gca, 'YScale', 'log')
+% ylabel('$\sqrt\epsilon_0/\omega = \sqrt {\frac{2\omega_0^3}{\omega}\frac{\Lambda}{\tilde N}}\Big/\omega$','Interpreter','latex','FontSize',fontsize+4)
+ylabel('${R_i}_\mathrm{min}$','Interpreter','latex','FontSize',fontsize+4)
 xlabel('$\omega_0 /\omega = (\tilde N \frac{k_0}{m_0})/\omega$','Interpreter','latex','FontSize',fontsize+4);
 title('Growth rate (hour$^{-1}$)','Interpreter','latex','FontSize',fontsize+4)
 box on;
 grid on;grid minor
 xlim([0 2.5])
+ylim([4 2000])
+
 
 load('fig5/fig5_topo4_Ri.mat')
 ax2 = axes('position',[0.55 0.15+0.002 0.39 0.77]);
 annotation('textbox',[0.49 0.99+0.002 0.15 0.01],'String','b','FontSize',fontsize+3,'fontweight','bold','LineStyle','None');
-scatter(omega0_sort/omega,sqrt(epsilon_sort)/omega,20,grow_sort,'o','filled');
-clim([0 0.024])
+% scatter(omega0_sort/omega,sqrt(epsilon_sort)/omega,20,grow_sort,'o','filled');
+scatter(omega0_sort/omega,Ri_sort,20,grow_sort,'o','filled');
+clim([0 0.04])
 % ylabel('$\sqrt\epsilon/\omega = \sqrt {2\Lambda\omega_0^3/(N\omega^3)}$','Interpreter','latex')
 % xlabel('$\sqrt\delta/\omega = \omega_0 /\omega = N k_x/m_z/\omega$','Interpreter','latex')
 set(gca,'fontsize',fontsize)
-ylabel('$\sqrt\epsilon_0/\omega = \sqrt {\frac{2\omega_0^3}{\omega}\frac{\Lambda}{\tilde N}}\Big/\omega$','Interpreter','latex','FontSize',fontsize+4)
+set(gca, 'YScale', 'log')
+% ylabel('$\sqrt\epsilon_0/\omega = \sqrt {\frac{2\omega_0^3}{\omega}\frac{\Lambda}{\tilde N}}\Big/\omega$','Interpreter','latex','FontSize',fontsize+4)
+ylabel('${R_i}_\mathrm{min}$','Interpreter','latex','FontSize',fontsize+4)
 xlabel('$\omega_0 /\omega = (\tilde N \frac{k_0}{m_0})/\omega$','Interpreter','latex','FontSize',fontsize+4)
 title('Growth rate (hour$^{-1}$)','Interpreter','latex','FontSize',fontsize+4)
 box on;
 grid on;grid minor
 h3 = colorbar(ax2,'Position',[0.95 0.1400+0.002 0.0160/2 0.7800]);
 xlim([0 2.5])
+ylim([4 2000])
 % print('-dpng','-r300','fig5/fig5_matlab.png');
 
--- instability_km/plot_Mathieu_new.m
+++ instability_km/plot_Mathieu_new.m
@@ -9,7 +9,14 @@ load([expdir '_output.mat'])
 grow_all = [];
 omega0_all = [];
 epsilon_all = [];
+Ri_all = [];
 omega0 = N*kx_all/m0_rw;
+
+dt_ri = 10;
+Nt_ri = 100*Ptide/dt_ri;
+tt_ri = dt_ri:dt_ri:Nt_ri*dt_ri;
+
+Ri_shear = NaN.*zeros(1,Ns); %%% minimum Ri as a function of shear
 
 for ns=1:Ns
     shear = shear_all(ns)
@@ -21,6 +28,18 @@ for ns=1:Ns
     grow_all = [grow_all grow];
 
     omega0_all = [omega0_all omega0];
+
+
+    %%% Compute Richardson number
+    Ri_inverse = (shear*cos(omega*tt_ri)).^2./(N^2*cosd(topo) - N^2*sind(topo)/omega*shear*sin(omega*tt_ri));
+    % figure(50);plot(tt_ri,Ri_inverse)
+    Ri_shear(ns) = 1/max(Ri_inverse);  
+    if(min(Ri_inverse)<0)
+        isConvec = 1
+    end
+
+    Ri_all = [Ri_all Ri_shear(ns).*ones(1,Nk)];
+
 end
 
 
@@ -54,9 +73,9 @@ end
 [omega0_sort,I] = sort(omega0_all);
 epsilon_sort = epsilon_all(I);
 grow_sort = grow_all(I);
+Ri_sort = Ri_all(I);
 
-
-save('../figures/fig5/fig5_topo0.mat')
+save('../figures/fig5/fig5_topo0_Ri.mat')
 
 
 
