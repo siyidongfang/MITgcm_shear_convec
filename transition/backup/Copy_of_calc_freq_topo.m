e6e2b1c8a0c6dc3e255319ebe400170818cc2f04
siyidongfang <y_si@mit.edu>
2024-09-02 18:57:23 -0400

    update

--- transition/backup/Copy_of_calc_freq_topo.m
+++ transition/backup/Copy_of_calc_freq_topo.m
@@ -13,7 +13,7 @@ tt_hat = 0:dt:2*pi*Ntide;
 Nt = length(tt_hat);
 
 % R_all = [2.1:0.1:6];
-R_all = [0:0.025:6];
+R_all = [0:0.1:4.4];
 nR = length(R_all);   
 om1 = zeros(1,nR);
 om2 = zeros(1,nR);
@@ -21,11 +21,16 @@ om3 = zeros(1,nR);
 nom1 = zeros(1,nR);
 nom2 = zeros(1,nR);
 nom3 = zeros(1,nR);
-sigma_new_idx = zeros(nR,Nt);
-sigma_new = zeros(nR,Nt);
+alpha2 = zeros(1,nR);
+alpha3 = zeros(1,nR);
+NT1 = zeros(1,nR);
+NT3 = zeros(1,nR);
+NT5 = zeros(1,nR);
+
 topo=4;
 
 for o=1:nR
+% for o=10
 
     R = R_all(o);
     r = cosd(topo)/sind(topo)-N_hat*sqrt(R); %%% m0/k0
@@ -47,49 +52,106 @@ for o=1:nR
     sigma(sigma==0)=NaN;
     sigma1_imag_harmonic(o) = 1/(sum(1./sigma,'omitnan')/length(sigma));
 
-    for n=1:Nt
-        om = sigma1_imag(n);
-        if(om>=1)
-           om1(o) = om1(o) + 1/om;
-           nom1(o) = nom1(o)+1;
-           sigma_new_idx(o,n)=1;
-        elseif(om<1 && om>=sigma1_imag_harmonic(o))
-           om2(o) = om2(o) + 1/om;
-           nom2(o) = nom2(o)+1;
-           sigma_new_idx(o,n)=2;
-        elseif(om>0 && om<sigma1_imag_harmonic(o))
-           om3(o) = om3(o) + 1/om;
-           nom3(o) = nom3(o)+1;
-           sigma_new_idx(o,n)=3;
-        elseif(om==0)
-           sigma_new_idx(o,n)=3;
+    if(R>2.45)
+
+        for n=1:Nt
+            om = sigma1_imag(n);
+            if(om~=0)
+                if(om>=1)
+                    om1(o) = om1(o) + 1/om;
+                    nom1(o) = nom1(o)+1;
+                    if(n<round(Nt/4))
+                        NT1(o) = NT1(o)+1;
+                    elseif(n>round(Nt/4) && n<round(Nt/4*3))
+                        NT3(o) = NT3(o)+1;
+                    else
+                        NT5(o) = NT5(o)+1;
+                    end
+                else
+                    if(n<round(Nt/2))
+                        om2(o) = om2(o) + 1/om;
+                        nom2(o) = nom2(o)+1;
+                    else
+                        om3(o) = om3(o) + 1/om;
+                        nom3(o) = nom3(o)+1;
+                    end
+                end
+            end
         end
-    end   
 
-    om1(o) = om1(o)/nom1(o);
-    om2(o) = om2(o)/nom2(o);
-    om3(o) = om3(o)/nom3(o);
-    om1(o) = 1/om1(o);
-    om2(o) = 1/om2(o);
-    om3(o) = 1/om3(o);
+        om1(o) = om1(o)/nom1(o);
+        om2(o) = om2(o)/nom2(o);
+        om3(o) = om3(o)/nom3(o);
+        om1(o) = 1/om1(o);
+        om2(o) = 1/om2(o);
+        om3(o) = 1/om3(o);
+        %%% Portion of a period (0~2*pi) with frequency lower than 1
+        alpha2(o) = nom2(o)/Nt;
+        alpha3(o) = nom3(o)/Nt;
 
-    o1=om1(o);
-    o2=om2(o);
-    o3=om3(o);
+        o1=om1(o);
+        o2=om2(o);
+        o3=om3(o);
+        al2 = alpha2(o);
+        al3 = alpha3(o);
+        beta = 1-(al2+al3);
+        t1 = NT1(o);
+        t2 = t1 + nom2(o);
+        t3 = t2 + NT3(o);
+        t4 = t3 + nom3(o);
+        t5 = Nt;
+    
+        sigma_new = [o1*ones(1,t1) o2*ones(1,t2-t1) o1*ones(1,t3-t2) o3*ones(1,t4-t3) o1*ones(1,t5-t4)];
 
-    ntransition(o) = sum(abs(diff(sigma_new_idx(o,:))));
-    transition_idx = find(diff(sigma_new_idx(o,:))~=0);
+    else
 
-    for n=1:Nt
-        aa=sigma_new_idx(o,n);
-        if(aa==1)
-            sigma_new(o,n)=o1;
-        elseif(aa==2)
-            sigma_new(o,n)=o2;
-        elseif(aa==3)
-            sigma_new(o,n)=o3;
+        for n=1:Nt
+            om = sigma1_imag(n);
+            if(om~=0)
+                if(n<round(Nt/2))
+                    om1(o) = om1(o) + 1/om;
+                    nom1(o) = nom1(o)+1;
+                else
+                    om3(o) = om3(o) + 1/om;
+                    nom3(o) = nom3(o)+1;
+                end
+            end
+            om2(o) = NaN;
+            nom2(o) = 0;
         end
+
+        om1(o) = om1(o)/nom1(o);
+        om2(o) = om2(o)/nom2(o);
+        om3(o) = om3(o)/nom3(o);
+        om1(o) = 1/om1(o);
+        om2(o) = 1/om2(o);
+        om3(o) = 1/om3(o);
+        %%% Portion of a period (0~2*pi) with frequency lower than 1
+        alpha2(o) = nom2(o)/Nt;
+        alpha3(o) = nom3(o)/Nt;
+
+        o1=om1(o);
+        o2=om2(o);
+        o3=om3(o);
+        al2 = alpha2(o);
+        al3 = alpha3(o);
+        beta = 1-(al2+al3);
+        t1 = NT1(o);
+        t2 = t1 + nom2(o);
+        t3 = t2 + NT3(o);
+        t4 = t3 + nom3(o);
+        t5 = Nt;
+    
+        sigma_new = [o1*ones(1,t4) o3*ones(1,t5-t4)];
+
+
+
     end
+
+
+
+
+
 
     % figure(1)
     % clf;set(gcf,'Color','w','Position',[41 146 831 753])
@@ -119,7 +181,7 @@ for o=1:nR
     figure(4);clf;set(gcf,'Color','w','Position',[41 146 1275 428])
     plot(tt_hat/2/pi,sigma1_imag,'LineWidth',2);
     hold on;
-    plot(tt_hat/2/pi,sigma_new(o,:),'LineWidth',2);
+    plot(tt_hat/2/pi,sigma_new,'LineWidth',2);
     plot(tt_hat/2/pi,sigma1_imag_harmonic(o)*ones(1,length(tt_hat)),'--','LineWidth',1.5);
     plot(tt_hat/2/pi,ones(1,length(tt_hat)),'--','LineWidth',2,'Color','k');
     grid on;grid minor;set(gca,'Fontsize',20);
@@ -129,6 +191,12 @@ for o=1:nR
     legend('$\sigma(\hat t)$','Idealized $\sigma$','harmonic mean of $\sigma(\hat t)$',...
         'Interpreter','latex','Position',[0.1447 0.1787 0.1565 0.1647]);
     ylim([1e-5 10])
+
+    nt1(o)=t1;
+    nt2(o)=t2;
+    nt3(o)=t3;
+    nt4(o)=t4;
+    nt5(o)=t5;
 
 end
 
@@ -149,9 +217,18 @@ grid on;grid minor;set(gca,'Fontsize',20);
 % ylim([0.1 10])
 xlabel('$R={R_i}_\mathrm{min}^{-1}$','Interpreter','latex');
 legend('$\omega_1$ = harmonic mean of $\sigma(\hat t)$ higher than 1',...
-    '$\omega_2$',...
-    '$\omega_3$',...
+    '$\omega_2$ = harmonic mean of $\sigma(\hat t)$ lower than 1 ($0<t<\pi$)',...
+    '$\omega_3$ = harmonic mean of $\sigma(\hat t)$ lower than 1 ($\pi<t<2\pi$)',...
     'Interpreter','latex');
 
+subplot(1,2,2)
+plot(R_all,alpha2,'LineWidth',2,'Color',orange)
+hold on;
+plot(R_all,alpha3,'LineWidth',2,'Color',green)
+plot(R_all,alpha2+alpha3,'k--','LineWidth',2)
+grid on;grid minor;set(gca,'Fontsize',20);
+xlabel('$R={R_i}_\mathrm{min}^{-1}$','Interpreter','latex');
+title('Portion of a period with $\sigma(\hat t)$ lower than 1','Interpreter','latex');
+legend('Portion of $\omega_2$','Portion of $\omega_3$','Portion of $\omega_2$ + Portion of $\omega_3$','Interpreter','latex');
 
 
--- transition/calc_grow_topo.m
+++ transition/calc_grow_topo.m
@@ -12,71 +12,113 @@ for o = 2:nR
     o1=om1(o);
     o2=om2(o);
     o3=om3(o);
-    al2 = alpha2(o);
-    al3 = alpha3(o);
-    beta = 1-(al2+al3);
 
-    t1 = NT1(o);
-    t2 = t1 + nom2(o);
-    t3 = t2 + NT3(o);
-    t4 = t3 + nom3(o);
-    t5 = Nt;
+
+    ntransition(o);
+    transition_idx = find(diff(sigma_new_idx(o,:))~=0);
+
+    if(ntransition(o)==2)
+        t1 = transition_idx(1);
+        t2 = transition_idx(2);
+        t3 = Nt;
+        tt1 = tt_hat(1:t1);
+        tt2 = tt_hat(t1+1:t2);
+        tt3 = tt_hat(t2+1:t3);
+        t1 = t1/Nt*2*pi;
+        t2 = t2/Nt*2*pi;
+        t3 = t3/Nt*2*pi;
+    elseif(ntransition(o)==4)
+        t1 = transition_idx(1);
+        t2 = transition_idx(2);
+        t3 = transition_idx(3);
+        t4 = transition_idx(4);
+        t5 = Nt;
+        tt1 = tt_hat(1:t1);
+        tt2 = tt_hat(t1+1:t2);
+        tt3 = tt_hat(t2+1:t3);
+        tt4 = tt_hat(t3+1:t4);
+        tt5 = tt_hat(t4+1:t5);
+        t1 = t1/Nt*2*pi;
+        t2 = t2/Nt*2*pi;
+        t3 = t3/Nt*2*pi;
+        t4 = t4/Nt*2*pi;
+        t5 = t5/Nt*2*pi;
+    elseif(ntransition(o)==6)
+        t1 = transition_idx(1);
+        t2 = transition_idx(2);
+        t3 = transition_idx(3);
+        t4 = transition_idx(4);
+        t5 = transition_idx(5);
+        t6 = transition_idx(6);
+        t7 = Nt;
+        tt1 = tt_hat(1:t1);
+        tt2 = tt_hat(t1+1:t2);
+        tt3 = tt_hat(t2+1:t3);
+        tt4 = tt_hat(t3+1:t4);
+        tt5 = tt_hat(t4+1:t5);
+        tt6 = tt_hat(t5+1:t6);
+        tt7 = tt_hat(t6+1:t7);
+        t1 = t1/Nt*2*pi;
+        t2 = t2/Nt*2*pi;
+        t3 = t3/Nt*2*pi;
+        t4 = t4/Nt*2*pi;
+        t5 = t5/Nt*2*pi;
+        t6 = t6/Nt*2*pi;
+        t7 = t7/Nt*2*pi;
+    end
 
     
-    tt1 = tt_hat(1:t1);
-    tt2 = tt_hat(t1+1:t2);
-    tt3 = tt_hat(t2+1:t3);
-    tt4 = tt_hat(t3+1:t4);
-    tt5 = tt_hat(t4+1:t5);
-    
-    t1 = t1/Nt*2*pi;
-    t2 = t2/Nt*2*pi;
-    t3 = t3/Nt*2*pi;
-    t4 = t4/Nt*2*pi;
-    t5 = t5/Nt*2*pi;
-    
-    
-    figure(1)
-    clf;set(gcf,'Color','w','Position',[41 146 1275 428])
-    hold on;
+    % figure(1)
+    % clf;set(gcf,'Color','w','Position',[41 146 1275 428])
+    % hold on;
     
     nd=1;
     zeta = [];
     tt = [];
     
-    ncycle = 7;
+    ncycle = 50;
     for k=1:ncycle
 
-        if(R<2.5)
+        if(ntransition(o)==2)
             if(k==1)
                a1=1;
                p1=0;
             else
-                clear a1 p1 p2 a2 p3 a3 p4 a4 
-                syms  a1 p1 p2 a2 p3 a3 p4 a4
-                eq1 = a1*cos(o1*((k-1)*2*pi+0)+p1)-a5*cos(o3*((k-1)*2*pi+0)+p5)==0;
-                eq2 = o1*a1*sin(o1*((k-1)*2*pi+0)+p1)-o3*a5*sin(o3*((k-1)*2*pi+0)+p5)==0;
-                S = solve(eq1,eq2);
-                p1 = double(S.p1(nd));
-                a1 =double(S.a1(nd));
+               a1=a3;
+               p1=p3;
             end
             
-            clear p2 a2 p3 a3 p4 a4 p5 a5
-            syms  p2 a2 p3 a3 p4 a4 p5 a5
+            clear p2 a2 p3 a3 p4 a4 p5 a5 a6 p6 a7 p7
+            syms  p2 a2 p3 a3 p4 a4 p5 a5 a6 p6 a7 p7
 
-            eq1 = a1*cos(o1*((k-1)*2*pi+t4)+p1)-a5*cos(o3*((k-1)*2*pi+t4)+p5)==0;
-            eq2 = o1*a1*sin(o1*((k-1)*2*pi+t4)+p1)-o3*a5*sin(o3*((k-1)*2*pi+t4)+p5)==0;
+            eq1 = a1*cos(o2*((k-1)*2*pi+t1)+p1)-a2*cos(o3*((k-1)*2*pi+t1)+p2)==0;
+            eq2 = o2*a1*sin(o2*((k-1)*2*pi+t1)+p1)-o3*a2*sin(o3*((k-1)*2*pi+t1)+p2)==0;
             S = solve(eq1,eq2);
-            p5 = double(S.p5(nd));
-            a5 =double(S.a5(nd));
-
-            z1=[];
-            z2=[];
-            z3=[];
-            z4 = a1*cos(o1*((k-1)*2*pi+tt4)+p1);
-            z5 = a5*cos(o3*((k-1)*2*pi+tt5)+p5);
+            p2 = double(S.p2(nd));
+            a2 =double(S.a2(nd));
             
-        else
+            eq3 = a2*cos(o3*((k-1)*2*pi+t2)+p2)-a3*cos(o2*((k-1)*2*pi+t2)+p3)==0;
+            eq4 = o3*a2*sin(o3*((k-1)*2*pi+t2)+p2)-o2*a3*sin(o2*((k-1)*2*pi+t2)+p3)==0;
+            S = solve(eq3,eq4);
+            p3 = double(S.p3(nd));
+            a3 =double(S.a3(nd));
+
+            z1 = a1*cos(o2*((k-1)*2*pi+tt1)+p1);
+            z2 = a2*cos(o3*((k-1)*2*pi+tt2)+p2);
+            z3 = a3*cos(o2*((k-1)*2*pi+tt3)+p3);
+            z4=[];
+            z5=[];
+            z6=[];
+            z7=[];
+
+            zeta = [zeta z1 z2 z3 z4 z5 z6 z7];
+            
+            tt = [tt (k-1)*2*pi+tt_hat];
+            % plot(k-1+tt1/2/pi,z1,'LineWidth',2,'Color',blue)
+            % plot(k-1+tt2/2/pi,z2,'LineWidth',2,'Color',orange)
+            % plot(k-1+tt3/2/pi,z3,'LineWidth',2,'Color',blue)
+            
+        elseif(ntransition(o)==4)
 
             if(k==1)
                a1=1;
@@ -86,8 +128,61 @@ for o = 2:nR
                p1=p5;
             end
             
-            clear p2 a2 p3 a3 p4 a4 p5 a5
-            syms  p2 a2 p3 a3 p4 a4 p5 a5
+            clear p2 a2 p3 a3 p4 a4 p5 a5 a6 p6 a7 p7
+            syms  p2 a2 p3 a3 p4 a4 p5 a5 a6 p6 a7 p7
+
+            eq1 = a1*cos(o2*((k-1)*2*pi+t1)+p1)-a2*cos(o1*((k-1)*2*pi+t1)+p2)==0;
+            eq2 = o2*a1*sin(o2*((k-1)*2*pi+t1)+p1)-o1*a2*sin(o1*((k-1)*2*pi+t1)+p2)==0;
+            S = solve(eq1,eq2);
+            p2 = double(S.p2(nd));
+            a2 =double(S.a2(nd));
+            
+            eq3 = a2*cos(o1*((k-1)*2*pi+t2)+p2)-a3*cos(o2*((k-1)*2*pi+t2)+p3)==0;
+            eq4 = o1*a2*sin(o1*((k-1)*2*pi+t2)+p2)-o2*a3*sin(o2*((k-1)*2*pi+t2)+p3)==0;
+            S = solve(eq3,eq4);
+            p3 = double(S.p3(nd));
+            a3 =double(S.a3(nd));
+            
+            eq5 = a3*cos(o2*((k-1)*2*pi+t3)+p3)-a4*cos(o3*((k-1)*2*pi+t3)+p4)==0;
+            eq6 = o2*a3*sin(o2*((k-1)*2*pi+t3)+p3)-o3*a4*sin(o3*((k-1)*2*pi+t3)+p4)==0;
+            S = solve(eq5,eq6);
+            p4 = double(S.p4(nd));
+            a4 =double(S.a4(nd));
+            
+            eq7 = a4*cos(o3*((k-1)*2*pi+t4)+p4)-a5*cos(o2*((k-1)*2*pi+t4)+p5)==0;
+            eq8 = o3*a4*sin(o3*((k-1)*2*pi+t4)+p4)-o2*a5*sin(o2*((k-1)*2*pi+t4)+p5)==0;
+            S = solve(eq7,eq8);
+            p5 = double(S.p5(nd));
+            a5 =double(S.a5(nd));
+
+            z1 = a1*cos(o2*((k-1)*2*pi+tt1)+p1);
+            z2 = a2*cos(o1*((k-1)*2*pi+tt2)+p2);
+            z3 = a3*cos(o2*((k-1)*2*pi+tt3)+p3);
+            z4 = a4*cos(o3*((k-1)*2*pi+tt4)+p4);
+            z5 = a5*cos(o2*((k-1)*2*pi+tt5)+p5);
+            z6=[];
+            z7=[];
+
+            zeta = [zeta z1 z2 z3 z4 z5 z6 z7];
+            
+            tt = [tt (k-1)*2*pi+tt_hat];
+            % plot(k-1+tt1/2/pi,z1,'LineWidth',2,'Color',blue)
+            % plot(k-1+tt2/2/pi,z2,'LineWidth',2,'Color',green)
+            % plot(k-1+tt3/2/pi,z3,'LineWidth',2,'Color',blue)
+            % plot(k-1+tt4/2/pi,z4,'LineWidth',2,'Color',orange)
+            % plot(k-1+tt5/2/pi,z5,'LineWidth',2,'Color',blue)
+
+        elseif(ntransition(o)==6)
+            if(k==1)
+               a1=1;
+               p1=0;
+            else
+               a1=a7;
+               p1=p7;
+            end
+            
+            clear p2 a2 p3 a3 p4 a4 p5 a5 a6 p6 a7 p7
+            syms  p2 a2 p3 a3 p4 a4 p5 a5 a6 p6 a7 p7
 
             eq1 = a1*cos(o1*((k-1)*2*pi+t1)+p1)-a2*cos(o2*((k-1)*2*pi+t1)+p2)==0;
             eq2 = o1*a1*sin(o1*((k-1)*2*pi+t1)+p1)-o2*a2*sin(o2*((k-1)*2*pi+t1)+p2)==0;
@@ -107,38 +202,54 @@ for o = 2:nR
             p4 = double(S.p4(nd));
             a4 =double(S.a4(nd));
             
-            eq7 = a4*cos(o2*((k-1)*2*pi+t4)+p4)-a5*cos(o1*((k-1)*2*pi+t4)+p5)==0;
-            eq8 = o2*a4*sin(o2*((k-1)*2*pi+t4)+p4)-o1*a5*sin(o1*((k-1)*2*pi+t4)+p5)==0;
+            eq7 = a4*cos(o2*((k-1)*2*pi+t4)+p4)-a5*cos(o3*((k-1)*2*pi+t4)+p5)==0;
+            eq8 = o2*a4*sin(o2*((k-1)*2*pi+t4)+p4)-o3*a5*sin(o3*((k-1)*2*pi+t4)+p5)==0;
             S = solve(eq7,eq8);
             p5 = double(S.p5(nd));
             a5 =double(S.a5(nd));
+
+            eq9 = a5*cos(o3*((k-1)*2*pi+t5)+p5)-a6*cos(o2*((k-1)*2*pi+t5)+p6)==0;
+            eq10 = o3*a5*sin(o3*((k-1)*2*pi+t5)+p5)-o2*a6*sin(o2*((k-1)*2*pi+t5)+p6)==0;
+            S = solve(eq9,eq10);
+            p6 = double(S.p6(nd));
+            a6 =double(S.a6(nd));
+            
+            eq11 = a6*cos(o2*((k-1)*2*pi+t6)+p6)-a7*cos(o1*((k-1)*2*pi+t6)+p7)==0;
+            eq12 = o2*a6*sin(o2*((k-1)*2*pi+t6)+p6)-o1*a7*sin(o1*((k-1)*2*pi+t6)+p7)==0;
+            S = solve(eq11,eq12);
+            p7 = double(S.p7(nd));
+            a7 =double(S.a7(nd));
 
             z1 = a1*cos(o1*((k-1)*2*pi+tt1)+p1);
             z2 = a2*cos(o2*((k-1)*2*pi+tt2)+p2);
             z3 = a3*cos(o1*((k-1)*2*pi+tt3)+p3);
             z4 = a4*cos(o2*((k-1)*2*pi+tt4)+p4);
-            z5 = a5*cos(o1*((k-1)*2*pi+tt5)+p5);
+            z5 = a5*cos(o3*((k-1)*2*pi+tt5)+p5);
+            z6 = a6*cos(o2*((k-1)*2*pi+tt6)+p6);
+            z7 = a7*cos(o1*((k-1)*2*pi+tt7)+p7);
+
+            zeta = [zeta z1 z2 z3 z4 z5 z6 z7];
+            
+            tt = [tt (k-1)*2*pi+tt_hat];
+            % plot(k-1+tt1/2/pi,z1,'LineWidth',2,'Color',green)
+            % plot(k-1+tt2/2/pi,z2,'LineWidth',2,'Color',blue)
+            % plot(k-1+tt3/2/pi,z3,'LineWidth',2,'Color',green)
+            % plot(k-1+tt4/2/pi,z4,'LineWidth',2,'Color',blue)
+            % plot(k-1+tt5/2/pi,z5,'LineWidth',2,'Color',orange)
+            % plot(k-1+tt6/2/pi,z6,'LineWidth',2,'Color',blue)
+            % plot(k-1+tt7/2/pi,z7,'LineWidth',2,'Color',green)
+
         end
         
-
-        
-        zeta = [zeta z1 z2 z3 z4 z5];
-        
-        tt = [tt (k-1)*2*pi+tt_hat];
-        plot(k-1+tt1/2/pi,z1,'LineWidth',2,'Color',green)
-        plot(k-1+tt2/2/pi,z2,'LineWidth',2,'Color',blue)
-        plot(k-1+tt3/2/pi,z3,'LineWidth',2,'Color',green)
-        plot(k-1+tt4/2/pi,z4,'LineWidth',2,'Color',blue)
-        plot(k-1+tt5/2/pi,z5,'LineWidth',2,'Color',green)
 
     end
 
 
-% ylim([-0.6 1.6]*1e4)
-grid on;grid minor;set(gca,'Fontsize',20);
-xlabel('Time, $\hat t/(2\pi)$ (Tidal cycles)','Interpreter','latex');
-title(['$\zeta(\hat t)$, R=' num2str(R,3)],'Interpreter','latex');
-print('-dpng','-r150',['figures_topo4/topo4_o' num2str(o) '.png']);
+% % ylim([-0.6 1.6]*1e4)
+% grid on;grid minor;set(gca,'Fontsize',20);
+% xlabel('Time, $\hat t/(2\pi)$ (Tidal cycles)','Interpreter','latex');
+% title(['$\zeta(\hat t)$, R=' num2str(R,3)],'Interpreter','latex');
+% print('-dpng','-r150',['figures_topo4/topo4_o' num2str(o) '.png']);
 
 ke = 0.5*zeta.^2;
 fit_span = 1:Nt*ncycle;
@@ -148,17 +259,17 @@ yyplot = log(ke)/2;
 grow(o)=pp(1);
 [y_fit,delta_fit] = polyval(pp,xxplot,S);
 
-figure(2)
-clf;set(gcf,'Color','w','Position',[41 146 1275 428])
-plot(tt/2/pi,log(ke)/2,'LineWidth',2)
-hold on;
-plot(xxplot(fit_span)/12, y_fit(fit_span));
-hold off;
-grid on;grid minor;set(gca,'Fontsize',20);
-ylabel('$\ln(\mathrm{TKE})/2$','Interpreter','latex');
-xlabel('Time, $\hat t/(2\pi)$ (Tidal cycles)','Interpreter','latex');
-title(['TKE$\sim0.5*\zeta^2(\hat t)$, R=' num2str(R,3)],'Interpreter','latex');
-print('-dpng','-r150',['figures_topo4/ke' num2str(o) '.png']);
+% figure(2)
+% clf;set(gcf,'Color','w','Position',[41 146 1275 428])
+% plot(tt/2/pi,log(ke)/2,'LineWidth',2)
+% hold on;
+% plot(xxplot(fit_span)/12, y_fit(fit_span));
+% hold off;
+% grid on;grid minor;set(gca,'Fontsize',20);
+% ylabel('$\ln(\mathrm{TKE})/2$','Interpreter','latex');
+% xlabel('Time, $\hat t/(2\pi)$ (Tidal cycles)','Interpreter','latex');
+% title(['TKE$\sim0.5*\zeta^2(\hat t)$, R=' num2str(R,3)],'Interpreter','latex');
+% print('-dpng','-r150',['figures_topo4/ke' num2str(o) '.png']);
 
 
 end
@@ -177,10 +288,9 @@ title('Growth rate (1/hour)','Interpreter','latex');
 
 
 % load('/Users/ysi/MITgcm_shear_convec/instability_km/exps_new/topo4_nu0_output.mat','grow_rw','shear_all')
-load('../instability_km/exps_new/topo4_nu0_output.mat','grow_rw','shear_all')
+load('../instability_km/exps_new/topo4_nu0_output.mat','rw_all','grow_rw','shear_all')
 load('../figures/fig4/Ri_topo4.mat')
 hold on;
-
 
 max_grow_km = max(grow_rw,[],2);
 for i=1:length(shear_all)
@@ -200,4 +310,4 @@ xlabel('R = ${R_i}_\mathrm{min}^{-1}$','interpreter','latex');
 legend('Analytical solution','Theory')
 
 
-% print('-dpng','-r150',['figures_topo4/grow_topo.png']);
+print('-dpng','-r150',['figures_topo4/grow_topo.png']);
