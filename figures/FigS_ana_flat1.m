01f54dffa7faa31faf465986e6a326d2506bb023
siyidongfang <y_si@mit.edu>
2024-09-20 11:57:28 -0400

    update

--- figures/FigS_ana_flat1.m
+++ figures/FigS_ana_flat1.m
@@ -11,48 +11,59 @@ set(gcf,'Color','w','Position', [-46 153 1339 800])
 
 tiledlay = tiledlayout(3,3);
 x = 0:0.1:5;
-
+xt = 1;
+yt = 0.5;
+str = '$R=1.7$';
 nexttile
-plot(x,sin(x),'LineWidth',2)
+plot(x,sin(x))
 grid on;grid minor;set(gca,'FontSize',fontsize)
-title('Horizontal vorticity perturbation $\zeta(\hat t)$','Interpreter','latex','FontSize',fontsize+5);
+title('Time-dependent frequency $\sigma(\hat t)$','Interpreter','latex','FontSize',fontsize+5);
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x+1))
 grid on;grid minor;set(gca,'FontSize',fontsize)
 title('Horizontal vorticity perturbation $\zeta(\hat t)$','Interpreter','latex','FontSize',fontsize+5);
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x+2))
 grid on;grid minor;set(gca,'FontSize',fontsize)
 title('Turbulent kinetic energy $\sim0.5*\zeta^2(\hat t)$','Interpreter','latex','FontSize',fontsize+5);
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x))
 grid on;grid minor;set(gca,'FontSize',fontsize)
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x+1))
 grid on;grid minor;set(gca,'FontSize',fontsize)
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x+2))
 grid on;grid minor;set(gca,'FontSize',fontsize)
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x))
 grid on;grid minor;set(gca,'FontSize',fontsize)
 xlabel('Time, $\hat t/(2\pi)$ (Tidal cycles)','Interpreter','latex');
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x+1))
 grid on;grid minor;set(gca,'FontSize',fontsize)
 xlabel('Time, $\hat t/(2\pi)$ (Tidal cycles)','Interpreter','latex');
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 nexttile
 plot(x,sin(x+2))
 grid on;grid minor;set(gca,'FontSize',fontsize)
 xlabel('Time, $\hat t/(2\pi)$ (Tidal cycles)','Interpreter','latex');
+text(xt,yt,str,'Interpreter','latex','FontSize',fontsize+3);
 
 
 tiledlay.TileSpacing = 'compact';
@@ -62,6 +73,7 @@ tiledlay.Padding = 'compact';
 
 
 AddLetters2Plots(fg1,'FontSize',fontsize+5,'FontWeight','normal')
+
 
 
 % print('-dpng','-r300',['fig_supp_new/figS_ana_flat1.png']);
